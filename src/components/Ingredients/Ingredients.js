b0ea5cc7878ea67f90f719d69ea0036d071c961f
Andrei Ohiienko <andreyohiienko@gmail.com>
2020-06-25 16:19:43 +0300

    Performance optimizations with useMemo()

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useReducer, useState, useEffect, useCallback } from 'react'
+import React, { useReducer, useEffect, useCallback, useMemo } from 'react'
 
 import IngredientForm from './IngredientForm'
 import IngredientList from './IngredientList'
@@ -55,7 +55,7 @@ const Ingredients = () => {
     console.log('rendering ingredients', userIngredients)
   }, [userIngredients])
 
-  const addIngredientHandler = (ingredient) => {
+  const addIngredientHandler = useCallback((ingredient) => {
     dispatchHttp({ type: 'SEND' })
     fetch('https://hooks-573e9.firebaseio.com/ingredients.json', {
       method: 'POST',
@@ -81,9 +81,9 @@ const Ingredients = () => {
       .catch((error) => {
         dispatchHttp({ type: 'ERROR', errorMessage: error.message })
       })
-  }
+  }, [])
 
-  const removeIngredientHandler = (ingredientId) => {
+  const removeIngredientHandler = useCallback((ingredientId) => {
     dispatchHttp({ type: 'SEND' })
     fetch(
       `https://hooks-573e9.firebaseio.com/ingredients/${ingredientId}.json`,
@@ -103,11 +103,21 @@ const Ingredients = () => {
       .catch((error) => {
         dispatchHttp({ type: 'ERROR', errorMessage: error.message })
       })
-  }
+  }, [])
 
-  const clearError = () => {
+  const clearError = useCallback(() => {
     dispatchHttp({ type: 'CLEAR' })
-  }
+  }, [])
+
+  const ingredientList = useMemo(
+    () => (
+      <IngredientList
+        ingredients={userIngredients}
+        onRemoveItem={removeIngredientHandler}
+      />
+    ),
+    [userIngredients, removeIngredientHandler],
+  )
 
   return (
     <div className="App">
@@ -118,13 +128,9 @@ const Ingredients = () => {
         onAddIngredient={addIngredientHandler}
         loading={httpState.loading}
       />
-
       <section>
         <Search onLoadIngredients={filteredIngredientsHandler} />
-        <IngredientList
-          ingredients={userIngredients}
-          onRemoveItem={removeIngredientHandler}
-        />
+        {ingredientList}
       </section>
     </div>
   )
